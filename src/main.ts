e0a1fb30197331e11f5b877ba64ce7666e305644
trungnt1 <trungnt1@greenglobal.vn>
2024-03-24 08:29:14 +0700

    add common configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Register Validation
+  app.useGlobalPipes(new ValidationPipe({}));
+
+  // Register Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Chat')
+    .setDescription('The chat API description')
+    .setVersion('1.0')
+    .addTag('chat')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
